18ac42aff91488227b683da4b0a5f64ec518fc0c
Muneebbhat <muneebbhat1990@gmail.com>
2022-01-03 21:08:23 +0530

    added another api

--- src/components/NewsType/BreakingNews.jsx
+++ src/components/NewsType/BreakingNews.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const BreakingNews = () => {
-    return (
-        <div className="herald-main-content col-lg-9 col-md-9 col-mod-main">
-          <div className="row">
-          <div style={{backgroundColor:'red'}}
-                className="herald-module col-lg-12 col-md-12 col-sm-12"
-                id="herald-module-1-0"
-                data-col="12"
-              >
-                <div className="herald-mod-wrap">
-                  <div className="herald-mod-head">
-                    <div className="herald-mod-title">
-                      <h2 className="h6 herald-mod-h herald-color">
-                        Breaking News
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-                <div className="row herald-posts row-eq-height">
-                  <article
-                    className="
+const BreakingNews = ({breakNews}) => {
+  return (
+    <div className="herald-main-content col-lg-9 col-md-9 col-mod-main">
+      <div className="row">
+        <div
+          className="herald-module col-lg-12 col-md-12 col-sm-12"
+          id="herald-module-1-0"
+          data-col="12"
+        >
+          <div className="herald-mod-wrap">
+            <div className="herald-mod-head">
+              <div className="herald-mod-title">
+                <h2 className="h6 herald-mod-h herald-color">Breaking News</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row herald-posts row-eq-height">
+            <article
+              className="
                       herald-lay-c
                       post-191 post
                       type-post
@@ -30,97 +28,87 @@ const BreakingNews = () => {
                       hentry
                       category-food-and
                     "
-                  >
-                    <div
-                      className="herald-post-thumbnail herald-format-icon-middle"
-                    >
-                      <a
-                        href="https://demo.mekshq.com/herald/?p=191"
-                        title="Pasta is the secret ingredient for a healthy lifestyle"
-                      >
-                        <img
-                          width="470"
-                          height="264"
-                          src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-470x264.jpg"
-                          className="
+            >
+              <div className="herald-post-thumbnail herald-format-icon-middle">
+                <a
+                  href="https://demo.mekshq.com/herald/?p=191"
+                  title="Pasta is the secret ingredient for a healthy lifestyle"
+                >
+                  <img
+                    width="470"
+                    height="264"
+                    src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-470x264.jpg"
+                    className="
                             attachment-herald-lay-b
                             size-herald-lay-b
                             wp-post-image
                           "
-                          alt=""
-                          loading="lazy"
-                         
-                          sizes="(max-width: 470px) 100vw, 470px"
-                        />
-                      </a>
+                    alt=""
+                    loading="lazy"
+                    sizes="(max-width: 470px) 100vw, 470px"
+                  />
+                </a>
+              </div>
+
+              <div className="entry-header">
+                <span className="meta-category">
+                  <a
+                    href="https://demo.mekshq.com/herald/?cat=4"
+                    className="herald-cat-4"
+                  >
+                  </a>
+                </span>
+
+                <h2 className="entry-title h3">
+                  <a href="https://demo.mekshq.com/herald/?p=191">
+                    Pasta is the secret ingredient for a healthy lifestyle
+                  </a>
+                </h2>
+                <div className="entry-meta">
+                  <div className="meta-item herald-date">
+                    <span className="updated">1 week ago</span>
+                  </div>
+                  <div className="meta-item herald-author">
+                    <div className="coauthors couauthors-icon">
+                      <span className="vcard author">
+                        <span className="fn">
+                          <a href="https://demo.mekshq.com/herald?author_name=meks1">
+                            Patrick Callahan
+                          </a>
+                        </span>
+                      </span>
+                      <span className="vcard author">
+                        <span className="fn">
+                          <a href="https://demo.mekshq.com/herald?author_name=meks2">
+                            Lisa Scholfield
+                          </a>
+                        </span>
+                      </span>
+                      <span className="vcard author">
+                        <span className="fn">
+                          <a href="https://demo.mekshq.com/herald?author_name=meks3">
+                            John Bergstein
+                          </a>
+                        </span>
+                      </span>
                     </div>
-
-                    <div className="entry-header">
-                      <span className="meta-category"
-                        ><a
-                          href="https://demo.mekshq.com/herald/?cat=4"
-                          className="herald-cat-4"
-                          >Food &amp; Drinks</a
-                        ></span
-                      >
-
-                      <h2 className="entry-title h3">
-                        <a href="https://demo.mekshq.com/herald/?p=191"
-                          >Pasta is the secret ingredient for a healthy
-                          lifestyle</a
-                        >
-                      </h2>
-                      <div className="entry-meta">
-                        <div className="meta-item herald-date">
-                          <span className="updated">1 week ago</span>
-                        </div>
-                        <div className="meta-item herald-author">
-                          <div className="coauthors couauthors-icon">
-                            <span className="vcard author"
-                              ><span className="fn"
-                                ><a
-                                  href="https://demo.mekshq.com/herald?author_name=meks1"
-                                  >Patrick Callahan</a
-                                ></span
-                              ></span
-                            ><span className="vcard author"
-                              ><span className="fn"
-                                ><a
-                                  href="https://demo.mekshq.com/herald?author_name=meks2"
-                                  >Lisa Scholfield</a
-                                ></span
-                              ></span
-                            >
-                            <span className="vcard author"
-                              ><span className="fn"
-                                ><a
-                                  href="https://demo.mekshq.com/herald?author_name=meks3"
-                                  >John Bergstein</a
-                                ></span
-                              ></span
-                            >
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="entry-content">
-                      <p>
-                        This is an example of a post with multiple authors to
-                        showcase full support with Co Authors Plus WordPress
-                        Plugin. Master cleanse mumblecore...
-                      </p>
-                    </div>
-                  </article>
-
-                 
+                  </div>
                 </div>
               </div>
+
+              <div className="entry-content">
+                <p>
+                  This is an example of a post with multiple authors to showcase
+                  full support with Co Authors Plus WordPress Plugin. Master
+                  cleanse mumblecore...
+                </p>
+              </div>
+            </article>
           </div>
         </div>
-            
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BreakingNews
+export default BreakingNews;
--- src/components/NewsType/NewsIndex.js
+++ src/components/NewsType/NewsIndex.js
@@ -1,13 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import BreakingNews from "./BreakingNews";
 import SideNews from "./SideNews";
 
 const NewsIndex = () => {
+  const [brNews, setBrNews] = useState([])
+  useEffect(() => {
+   const getBreakingNews = async()=>{
+     const response = await axios.get(`https://newsdata.io/api/1/news?apikey=pub_32560f2f2851336f61200ac6123373bd4211&q=india`)
+     setBrNews(response.data.results);
+
+   }
+   getBreakingNews()
+  }, [])
   return (
     <div className="herald-section container" id="herald-section-1">
-      <div className="row">
-        
-        <BreakingNews />
+      <div className="row"> 
+        <BreakingNews breakNews={brNews}/>
         <SideNews/>
       </div>
     </div>
