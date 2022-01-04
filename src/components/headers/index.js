8bbbd14c42d6b7375d480043c04732971e668a1f
Sawar-Nabi <sawarnabi100@gmail.com>
2022-01-04 14:40:42 +0530

    Lazy Loading Added

--- src/components/headers/index.js
+++ src/components/headers/index.js
@@ -1,27 +1,30 @@
-import React from "react"
-import BottomHeader from "./BottomHeader"
-import MiddleHeader from "./MiddleHeader"
-import MobileNavigation from "./MobileNavigation"
-import ResponsiveHeader from "./ResponsiveHeader"
-import StickyHeader from "./StickyHeader"
-import TopHeader from "./TopHeader"
-import TrendingHeader from "./TrendingHeader"
+import React, { Suspense } from "react";
+import MobileNavigation from "./MobileNavigation";
+import ResponsiveHeader from "./ResponsiveHeader";
+import StickyHeader from "./StickyHeader";
+import TrendingHeader from "./TrendingHeader";
+import Loader from "../loader/Loader";
 
-const Headers = ({topNews}) => {
-    return (
-        <React.Fragment>
-            <header className="herald-site-header">
-                <TopHeader/>
-                <MiddleHeader/>
-                <BottomHeader/>
-                <TrendingHeader sixNews={topNews}/>
-                
-            </header>
-            <StickyHeader/>
-            <ResponsiveHeader/>
-            <MobileNavigation/>
-        </React.Fragment>
-    )
-}
+const TopHeader = React.lazy(() => import("./TopHeader"));
+const MiddleHeader = React.lazy(() => import("./MiddleHeader"));
+const BottomHeader = React.lazy(() => import("./BottomHeader"));
 
-export default Headers
+const Headers = ({ topNews }) => {
+  return (
+    <React.Fragment>
+      <header className="herald-site-header">
+        <Suspense fallback={<Loader />}>
+          <TopHeader />
+          <MiddleHeader />
+          <BottomHeader />
+        </Suspense>
+        <TrendingHeader sixNews={topNews} />
+      </header>
+      <StickyHeader />
+      <ResponsiveHeader />
+      <MobileNavigation />
+    </React.Fragment>
+  );
+};
+
+export default Headers;
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,15 @@
+import "../../styles/loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader_container">
+      <div class="loader">
+        <div class="big-circle">
+          <div class="small-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
